05954ca5bb5fbc241ca76d108cee87e9ef63b65c
Wagner <wagnersr92@gmail.com>
2018-01-18 22:04:59 -0200

    init

---
+++ app/controllers/assistant_controller.rb
@@ -0,0 +1,22 @@
+class AssistantController < ApplicationController
+  def messenger
+    query = (params[:result][:parameters]['any'].present?)? params[:result][:parameters]['any'] : "Ruby On Rails"
+    page = WikipediaConsulter.find_by_name(query)
+
+    if page.text.present?
+      text = "#{page.text.slice(0, 150)}... \b link completo: #{page.fullurl}"
+    else
+      text = "Não encontrei essa página na Wikipedia em inglês :("
+    end
+
+    response =
+    {
+      "speech": text,
+      "displayText": text,
+      "data": "",
+      "source": "Programming Assitant"
+    }
+
+    render json: response
+  end
+end
